7f77dbbb19bcfa235fa1d560468294700a1f829f
Amanda C <amandan.chavez@gmail.com>
2017-02-02 01:16:34 -0700

    change made to html css js

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 // target the input field where the user enters thier guess.
 //get the value of the input field
 //display the value on the screen
+var randomNumber = 0;
+var min = 1;
+var max = 100;
+
 
 var inputField = document.querySelector(".GET-guess");
 var guessDisplay = document.querySelector(".guess-display");
 var guessButton = document.querySelector(".guess-button");
 var clearButton = document.querySelector(".clear-button");
 var resetButton = document.querySelector(".reset-button");
+var messageToUser = document.querySelector(".message-to-user");
 
+window.onload = function () {
+  console.log("Initializing...")
+  randomNumber = generateRandomNumber(min, max);
+  console.log("Random number: " + randomNumber);
+  resetButton.disabled = true;
+  clearButton.disabled = true;
+  guessButton.disabled = true;
+}
 //Display message too high, low, or boom when conditional runs
 //var = messageToUser document.querySelector(".message-to-user");
 function showGuess () {
@@ -16,36 +29,56 @@ function showGuess () {
   console.log(displayValue);
 }
 
-function generateRandomNumber() {
-
+function generateRandomNumber(minimum, maximum) {
+  return Math.floor(Math.random()*(maximum+1-minimum) + minimum);
 }
-//page refresh -- what is this... how do I make it work
-window.onload = function () {
-  generateRandomNumber (min, max);
-}
-
-var randomNumber = Math.floor(Math.random() * 100)
 
 guessButton.addEventListener("click", function() {
-  console.log("hi")
-  showGuess()
+  console.log("guess received");
+  resetButton.disabled = false;
+  showGuess();
   console.log(randomNumber);
+  if (parseInt(inputField.value, 10)){
+    var userNum = parseInt(inputField.value, 10);
+    if (userNum == randomNumber){
+      messageToUser.innerText = "Boom!";
+    } else if (userNum < randomNumber && userNum >= min){
+      messageToUser.innerText = "Your number is too low";
+    } else if (userNum > randomNumber && userNum <= max) {
+      messageToUser.innerText = "Your number is too high";
+    } else {
+      alert("Enter a number between 1 and 100");
+    }
+  } else {
+    alert("Enter a number");
+  }
+  inputField.value = "";
+  clearButton.disabled = true;
+  guessButton.disabled = true;
 })
-
+//isNaN built in JS method to test whether argument is not a number
 clearButton.addEventListener("click", function() {
-  console.log("yay")
-  inputField.value = ''
+  console.log("yay");
+  inputField.value = "";
 })
 
+document.addEventListener("keyup", function() {
+  clearButton.disabled = false;
+  guessButton.disabled = false;
+})
 
 resetButton.addEventListener("click", function() {
   console.log("reset")
-  randomNumber = Math.floor(Math.random() * 100)
+  messageToUser.innerText = "Try again!";
+  guessDisplay.innerText = "";
+  inputField.value = "";
+  randomNumber = generateRandomNumber(min, max);
   console.log(randomNumber);
   //clear input field
   //clear the p tag with guess
   //generate a new random number
 })
+
 
 
 //take user input and compare it to random number
